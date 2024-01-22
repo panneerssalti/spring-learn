64966000d0f243ff0e46b459cce2e44e63f85c05
Panneer Selvam Shanmugam <panneerss.altimetrik@gmail.com>
2024-01-22 07:37:32 +0530

    scheduling-2

--- src/main/java/com/panneer/springlearn/scheduler/SchedulerJob.java
+++ src/main/java/com/panneer/springlearn/scheduler/SchedulerJob.java
@@ -1,2 +1,63 @@
-package com.panneer.springlearn.scheduler;public class SchedulerJob {
+package com.panneer.springlearn.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Component("SchedulerJob")
+@EnableScheduling
+//@EnableAsync
+public class SchedulerJob {
+
+    @Scheduled(initialDelay = 5000,fixedDelay = 1000)
+    void runSchedulerJobOne() throws InterruptedException {
+        var current = LocalDateTime.now();
+        var dateTimeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        log.info("current date in runSchedulerJobOne() is [{}]", current.format(dateTimeFormat));
+        Thread.sleep(5000);
+    }
+
+    @Scheduled(initialDelay = 5000,fixedRate = 1000)
+    void runSchedulerJobTwo() throws InterruptedException {
+        var current = LocalDateTime.now();
+        var dateTimeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        log.info("current date in runSchedulerJobTwo() is [{}]", current.format(dateTimeFormat));
+        Thread.sleep(5000);
+    }
+
+//    @Async
+    @Scheduled(initialDelay = 5000,fixedRate = 1000)
+    void runSchedulerJobThree() throws InterruptedException {
+        var current = LocalDateTime.now();
+        var dateTimeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        log.info("current date in runSchedulerJobThree() is [{}]", current.format(dateTimeFormat));
+        Thread.sleep(5000);
+    }
+
+
+    @Scheduled(cron = "*/5 * * * * *")
+    void runSchedulerJobFour() throws InterruptedException {
+        var current = LocalDateTime.now();
+        var dateTimeFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        log.info("current date in runSchedulerJobFour() is [{}]", current.format(dateTimeFormat));
+        Thread.sleep(5000);
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler(){
+        var threadPookTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPookTaskScheduler.setThreadNamePrefix("sample-thread");
+        threadPookTaskScheduler.setPoolSize(20);
+        return threadPookTaskScheduler;
+    }
 }
