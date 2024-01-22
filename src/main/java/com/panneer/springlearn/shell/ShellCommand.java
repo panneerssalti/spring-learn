999af336627dcd74444542c9872954525d47a50b
Panneer Selvam Shanmugam <panneerss.altimetrik@gmail.com>
2024-01-22 10:08:28 +0530

    scheduling-4

---
+++ src/main/java/com/panneer/springlearn/shell/ShellCommand.java
@@ -0,0 +1,35 @@
+package com.panneer.springlearn.shell;
+
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+@ShellComponent
+@ConditionalOnProperty(name = "shell.enabled", matchIfMissing = true)
+public class ShellCommand {
+
+    private final ShellCommandClient shellCommandClient;
+
+    public ShellCommand(ShellCommandClient shellCommandClient) {
+        this.shellCommandClient = shellCommandClient;
+    }
+
+    @ShellMethod(key = "hello", value = "this is a example of saying hello")
+    public String hello(@ShellOption(defaultValue = "Panneer") String arg){
+        return String.join(" ","Saying hello",arg);
+    }
+
+    @ShellMethod(key = "goodbye", value = "this is a example of saying goodbye")
+    public String goodBye(){
+        return "Saying goodBye";
+    }
+
+    @ShellMethod(key = "dadjoke", value = "this says dad joke")
+    public String dadJoke(){
+        var dadjoke = shellCommandClient.random();
+        return dadjoke.joke();
+    }
+
+}
---
+++ src/main/java/com/panneer/springlearn/shell/model/DadJokeResponse.java
@@ -0,0 +1,4 @@
+package com.panneer.springlearn.shell.model;
+
+public record DadJokeResponse(String id,String joke,Integer status) {
+}
