8492c94569fd321455541b620fafe87710661983
Panneer Selvam Shanmugam <panneerss.altimetrik@gmail.com>
2024-01-23 09:35:53 +0530

    update

--- src/main/java/com/panneer/springlearn/security/enums/Permission.java
+++ src/main/java/com/panneer/springlearn/security/enums/Permission.java
@@ -9,7 +9,7 @@ public enum Permission {
     USER1_READ("user1:read"),
     USER1_UPDATE("user1:update"),
     USER1_CREATE("user1:create"),
-    USER1_DELETE("user2:delete"),
+    USER1_DELETE("user1:delete"),
     USER2_READ("user2:read"),
     USER2_UPDATE("user2:update"),
     USER2_CREATE("user2:create"),
